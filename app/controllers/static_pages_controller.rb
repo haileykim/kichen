0479597c9310b83f06efd40aca6f47e2621dbdf7
Hailey Kim <heawon.hailey@gmail.com>
2014-10-09 21:06:42 +0900

    Design forms

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-  	@recipes = Recipe.limit(3)
+  	@recipes = Recipe.limit(3).order("created_at DESC").all
   	if current_user
       redirect_to recipes_path
     end
