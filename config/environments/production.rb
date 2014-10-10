954b7252ff760e008efd466ea244de5069221b49
Hailey Kim <heawon.hailey@gmail.com>
2014-10-10 19:58:58 +0900

    fix another action mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_setting = {
-    address: "smtp.mandrillapp.com",
-    port: 587,
-    enable_starttls_auto: true,
-    user_name: "lee.jungdo@gmail.com",
-    password: "wtywCbaYhOIIZWCIgs2CcQ",
-    authentication: "login"
-  }
   config.action_mailer.perform_deliveries = true
-  Rails.application.routes.default_url_options[:host] = "haileyskitchen.herokuapp.com"
+  config.action_mailer.default_url_options = { host: "haileyskitchen.herokuapp.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
