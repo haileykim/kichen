7743e013556fc4a37337bad4b7490766e1946010
Hailey Kim <heawon.hailey@gmail.com>
2014-10-10 19:56:02 +0900

    fix mailer host issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  # +++ Mailer
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
@@ -73,6 +74,7 @@ Rails.application.configure do
     authentication: "login"
   }
   config.action_mailer.perform_deliveries = true
+  Rails.application.routes.default_url_options[:host] = "haileyskitchen.herokuapp.com"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
